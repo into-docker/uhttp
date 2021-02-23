6709b57a4faca14bcd292a2d7e94f572af2199a7
Yannick Scherer <yannick.scherer@futurice.com>
2021-02-23 09:28:25 +0100

    Update LICENSE and dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject uhttp "0.1.0-SNAPSHOT"
   :description "Native CLI tool created using GraalVM and unixsocket-http."
   :url "https://github.com/into-docker/uhttp"
-  :license {:name "MIT License"
-            :url "none"
+  :license {:name "MIT"
+            :url "https://choosealicense.com/licenses/mit"
             :year 2020
-            :key "mit"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [unixsocket-http "1.0.4"]]
+            :key "mit"
+            :comment "MIT License"}
+  :dependencies [[org.clojure/clojure "1.10.2"]
+                 [unixsocket-http "1.0.7"]]
 
   :profiles {:uberjar {:uberjar-name "uhttp.jar"
                        :global-vars  {*assert* false}
